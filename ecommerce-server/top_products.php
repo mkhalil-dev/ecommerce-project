86fa5dfb5605ae2a8a88f35480a1f849434417fd
tonisfaddoul <tonisfaddoul@gmail.com>
2022-09-23 15:24:48 +0300

    fixed top 5 products

--- ecommerce-server/top_products.php
+++ ecommerce-server/top_products.php
@@ -2,7 +2,7 @@
 
 include("connection.php");
 
-$query = $mysqli->prepare("SELECT * FROM products order by view desc limit:5");
+$query = $mysqli->prepare("SELECT * FROM products order by view desc limit 5");
 $query->execute();
 $array = $query->get_result();
 
