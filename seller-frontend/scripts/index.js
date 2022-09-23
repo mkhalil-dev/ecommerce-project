7e232f2ab5557a8da2f53bfd0426c5464b664f81
Ghassan Fares <gfares@gmail.com>
2022-09-23 22:23:42 +0300

    js3 v2

--- seller-frontend/scripts/index.js
+++ seller-frontend/scripts/index.js
@@ -25,3 +25,27 @@ new Chart("myChart", {
 
 
 // sidebar buttons show - hide
+
+var xValues = ["Italy", "France", "Spain", "USA", "Argentina"];
+var yValues = [35, 49, 44, 24, 15];
+var barColors = ["red", "green", "blue", "orange", "brown"];
+
+new Chart("myChart", {
+    type: "bar",
+    data: {
+        labels: xValues,
+        datasets: [{
+            backgroundColor: barColors,
+            data: yValues
+        }]
+    },
+    options: {
+        legend: { display: false },
+        title: {
+            display: true,
+            text: "World Wine Production 2018"
+        }
+    }
+});
+
+/
