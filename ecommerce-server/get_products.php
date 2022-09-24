1636a13ce761c5feb3459fc20e95c408a524105b
mkhalil1997 <112473296+mkhalil-dev@users.noreply.github.com>
2022-09-24 12:30:35 +0300

    Added cat listing for products

--- ecommerce-server/get_products.php
+++ ecommerce-server/get_products.php
@@ -1,8 +1,26 @@
 <?php
 
 include('connection.php');
+ini_set('display_errors', 1);
 
-$query = $mysqli->prepare("SELECT * FROM products");
+if (isset($_POST['seen'])){
+    $seen = $_POST['seen'];
+}
+else{
+    $seen = "('')";
+}
+
+if (isset($_POST['catid'])){
+    $catid = $_POST['catid'];
+}
+$seen = "(".$seen.")";
+
+if(!isset($catid)){
+    $query = $mysqli->prepare("SELECT P.*,subquery.* FROM products P, (SELECT COUNT(C.id) as product_count FROM products C) AS subquery WHERE P.id NOT IN $seen LIMIT 8");
+}
+else{
+    $query = $mysqli->prepare("SELECT P.*,subquery.* FROM products P, (SELECT COUNT(C.id) as product_count FROM products C) AS subquery WHERE P.id NOT IN $seen AND P.categories_id = '$catid' LIMIT 8");
+}
 $query->execute();
 $result = $query->get_result();
 
@@ -10,4 +28,12 @@ while($product = $result->fetch_assoc()){
     $response[] = $product;
 }
 
-echo json_encode($response);
+if(isset($response)){
+    echo json_encode($response);
+}else {
+    $response = [
+        "success" => false,
+        "message" => "no products found"
+    ];
+    echo json_encode($response);
+}
