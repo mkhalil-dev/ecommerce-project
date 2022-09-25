22770e260e923e478eef9b182db6f11884dcccf9
tonisfaddoul <tonisfaddoul@gmail.com>
2022-09-25 16:05:39 +0300

    added seller views

---
+++ ecommerce-server/seller_views.php
@@ -0,0 +1,35 @@
+<?php
+
+include('connection.php');
+
+if (isset($_POST['seller_id'])) {
+    $seller_id = $_POST['seller_id'];
+} else {
+    $response = [
+        "success" => false,
+        "message" => "missing post elements"
+    ];
+    echo json_encode($response);
+    exit();
+}
+
+$query = $mysqli->prepare("SELECT view FROM products WHERE seller_id=?");
+$query->bind_param('s', $seller_id);
+$query->execute();
+$result = $query->get_result();
+
+
+while ($product = $result->fetch_assoc()) {
+    $response[] = $product;
+}
+
+if (isset($response)) {
+    $views = 0;
+    foreach ($response as $x) {
+        $views += $x['view'];
+    };
+    $results[] = $views;
+} else {
+    $results[] = 0;
+}
+echo json_encode($results);
