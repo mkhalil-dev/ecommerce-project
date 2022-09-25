5da8df01d7fde58f24fbfd20ac3bab1abc006383
mkhalil1997 <112473296+mkhalil-dev@users.noreply.github.com>
2022-09-25 23:13:22 +0300

    Completed admin Dashboard

--- admin-electron/admin/index.js
+++ admin-electron/admin/index.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d7c6fb29f21f58d8b9a2cedbca347e25b710017b4b015a3f3072b89f19097dfd
-size 8077
+oid sha256:e7abe37309d96a58ba79b4e1bfcc4784491c5c50bb50305807f8c580a56b7fdd
+size 10317
