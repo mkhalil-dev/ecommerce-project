92b2530a4f5a99c846958b9723efad0295e4156c
mkhalil1997 <112473296+mkhalil-dev@users.noreply.github.com>
2022-09-25 22:37:55 +0300

    Finished Sellers Remove

--- admin-electron/admin/index.js
+++ admin-electron/admin/index.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e3014a4c6637cf8785120ef1791473744a22383dbbd1926ddb82b11fbeffc225
-size 7063
+oid sha256:d7c6fb29f21f58d8b9a2cedbca347e25b710017b4b015a3f3072b89f19097dfd
+size 8077
