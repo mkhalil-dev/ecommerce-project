4e60c4cc639aa1b7158d8a501bc8e7c48300ca16
mkhalil1997 <112473296+mkhalil-dev@users.noreply.github.com>
2022-09-26 10:11:37 +0300

    Ads

--- client-frontend/scripts/index.js
+++ client-frontend/scripts/index.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7ec91e5f4e1966aef3ed15b5abcabdc09215c87959ebbd8756316294d06d3be0
-size 27419
+oid sha256:d72651cb45cfd661c785ef765cca279908aa0c04e716d9ab70fed6757dc49755
+size 27416
